285af3f91f880267428247fc7c99aa9e3e3a32a1
mtdev404 <michal.tyszko@gmail.com>
2020-06-09 10:08:02 +0200

    setup

---
+++ src/components/paragraph/Paragraph.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from '../../App.module.sass';
+
+const Paragraph = () => (
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
+    consectetur similique inventore cupiditate, iusto, delectus saepe id quia
+    aperiam quisquam debitis repellendus aliquid exercitationem excepturi non
+    quaerat itaque quasi in. Optio odio provident dolorem dolore id perspiciatis
+    alias natus? Consectetur quo maxime earum. Voluptate laborum in esse maiores
+    voluptas neque autem at enim, mollitia, quisquam error! Fugit, quisquam.
+    Dolor expedita voluptates nobis commodi cum hic adipisci laboriosam rerum,
+    modi et cumque culpa dolorem sapiente quam optio quisquam nostrum ullam
+    voluptate. Earum sunt debitis accusantium unde, at quae provident voluptatem
+    veritatis quaerat fugiat assumenda minima, esse blanditiis eos
+    exercitationem eaque impedit.
+  </p>
+);
+
+export default Paragraph;
