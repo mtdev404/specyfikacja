174478330d0e035b2d5a80e76f34856a7407eca9
mtdev404 <michal.tyszko@gmail.com>
2020-06-09 13:43:08 +0200

    sections nav data

---
+++ src/components/data/sections.js
@@ -0,0 +1,18 @@
+const SECTIONS = [
+  {
+    section: 'ogólne warunki dostarczania formatów reklamowych',
+    subsections: [],
+  },
+  { section: 'display', subsections: ['web', 'mobile'] },
+  {
+    section: 'mailing',
+    subsections: [
+      'e-mailing',
+      'video e-mailing',
+      'real-time e-mailing / e-mailing z zegarem/licznikiem',
+      'video landing page',
+      'dobre praktyki w e-mailingu',
+    ],
+  },
+  { section: 'SMS/MMS', subsections: [] },
+];
